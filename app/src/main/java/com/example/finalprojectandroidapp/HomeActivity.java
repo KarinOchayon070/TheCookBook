9df16631abbe41203e0e0883b1319e57e29ac6bc
KarinOchayon070 <karinoch070@gmail.com>
2022-12-13 20:36:15 +0200

    Initial

---
+++ app/src/main/java/com/example/finalprojectandroidapp/HomeActivity.java
@@ -0,0 +1,12 @@
+package com.example.finalprojectandroidapp;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        getSupportActionBar().hide();
+    }
+}
